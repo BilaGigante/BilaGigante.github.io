12593bf863d18642c2ba79a3e6f966c1a8bfceb0
BilaGigante <50372366+BilaGigante@users.noreply.github.com>
2019-06-03 17:41:00 -0300

    Add files via upload

---
+++ Jogoexe/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1559594370,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/ship-sheet0.png",
+		"images/firebutton-sheet0.png",
+		"images/myfire-sheet0.png",
+		"images/up-sheet0.png",
+		"images/roght-sheet0.png",
+		"images/ieft-sheet0.png",
+		"images/down-sheet0.png",
+		"images/background-sheet0.png",
+		"images/background-sheet1.png",
+		"images/enemy-sheet0.png",
+		"images/powerup-sheet0.png"
+	]
+}
